54b436633ea3dacc6778d77ff5e4f08174cdcb44
TuriniMattia <turinimattia96@gmail.com>
2024-03-05 17:32:18 +0100

    [add]commento-js

---
+++ app.js
@@ -0,0 +1,9 @@
+//creare un form che calcoli importo del viaggio base a kilometri della tratta e età del passeggero
+// 1-Attribuire all'Id KM-road di chiedere i km di tratta del biglietto
+    
+// 2-Attribuire all'Id CS-age di selezionare 1 delle 3 categorie di età 
+// 3-Calcolare il costo del biglietto  tramite Km-road * 0,21
+// 4-SE l'età selezionata con  CS-age è minorenne applicare 20% di sconto
+// 5-ALTRIMENTI SE l'età selezionata con Cs-age è over 65 applicare il 40% di sconto
+// 6-ALTRIMENTI stampare il risultato  senza sconti
+// 7-Stampare il risultato in un form a cui attribuiremo il valore id Ticket
